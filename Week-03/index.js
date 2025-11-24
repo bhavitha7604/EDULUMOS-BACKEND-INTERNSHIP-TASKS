3291e38cfab55673db14105c987a72fd7d51fae2
bhavitha7604 <bhavitha1976@gmail.com>
2025-11-24 13:08:02 +0530

    Add files via upload

---
+++ Week-03/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import usersRoutes from './routes/users.js';
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+app.use('/users', usersRoutes);
+
+app.get('/', (req, res) => res.send('Hello from homepage.'));
+
+app.listen(PORT, () =>
+  console.log(`Server running at http://localhost:${PORT}`)
+);
